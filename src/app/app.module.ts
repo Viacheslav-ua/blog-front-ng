b87abdc85525c4f847468fafc4a7b32a5675f6dc
Viacheslav-ua <atph@ukr.net>
2022-11-16 07:27:11 +0200

    Add NGRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { WebsiteModule } from './routing/website/website.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -15,6 +21,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RouterModule.forRoot([]),
     WebsiteModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
